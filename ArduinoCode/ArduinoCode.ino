01f54adfa8ea13296541dd980415899d26bb7df4
Thijs van Berkel <thijsvanberkel2004@gmail.com>
2024-10-04 11:05:56 +0200

    Arduino can now read incoming bytes from usb to ttl adapter

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -2,14 +2,33 @@ int input = 0;
 int previnput = 0;
 
 void setup() { 
-  pinMode(4, input);
+  pinMode(4, INPUT_PULLUP);
+  
   Serial.begin(9600);
+  Serial.println("Started");
+}
+
+int readByte() {
+  int byte = 0;
+  for(int i = 0; i < 9; i++) {
+    int input = digitalRead(4);
+    if(i != 0) {
+      byte += input << i - 1;
+    }
+    Serial.println("Bit read:");
+    Serial.println(input);
+    delay(9);
+  }
+  return byte;
 }
 
 void loop() {
   input = digitalRead(4);
-  if (input != previnput) {
-    previnput = input;
-    Serial.println(input);
+  
+  if (input == 0) {
+    Serial.println("Start reading");
+    int byte = readByte();
+    Serial.println(byte);
+    Serial.println("End reading");
   }
 }
